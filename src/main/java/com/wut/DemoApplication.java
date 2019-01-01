e580cc8e68ba87c5fc4c486a00a845d74cb2ae86
LiuFK <992036303@qq.com>
2019-01-01 22:19:37 +0800

    增加web.xml

--- src/main/java/com/wut/DemoApplication.java
+++ src/main/java/com/wut/DemoApplication.java
@@ -1,14 +1,21 @@
 package com.wut;
 
+import javafx.application.Application;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
 @MapperScan("com.wut.dao")
-public class DemoApplication extends SpringBootServletInitializer {
+public class DemoApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(DemoApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
